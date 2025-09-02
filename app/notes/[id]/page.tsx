1274c70c273efe732d6da1b7128df25841c81a2b
YURII VORONYCH <voronich97@gmail.com>
2025-09-02 16:46:00 +0300

    Initial commit

---
+++ app/notes/[id]/page.tsx
@@ -0,0 +1,26 @@
+import { fetchNoteById } from "@/lib/api";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+import NoteDetailsClient from "./NoteDetails.client";
+
+interface TaskDetailProps {
+  params: Promise<{ id: string }>;
+}
+const TaskDetails = async ({ params }: TaskDetailProps) => {
+  const { id } = await params;
+  const queryClient = new QueryClient();
+  queryClient.prefetchQuery({
+    queryKey: ["note", id],
+    queryFn: () => fetchNoteById(id),
+  });
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NoteDetailsClient />
+    </HydrationBoundary>
+  );
+};
+
+export default TaskDetails;
